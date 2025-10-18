d3fdd8df4eb9df35a84e954437611fac594a6b61
SATO Daisuke <densuke+gmail@gmail.com>
2025-10-19 07:34:07 +0900

    [WIP]CSRF対策によるエラー修正

--- src/web/csrf_protection.py
+++ src/web/csrf_protection.py
@@ -1,6 +1,7 @@
+import functools
 import http.cookies
 import secrets
-from typing import Optional
+from typing import List, Optional
 
 from itsdangerous import URLSafeSerializer
 from starlette.datastructures import MutableHeaders
@@ -65,7 +66,7 @@ class CSRFCookieMiddleware:
         state["csrf_token"] = token
 
         async def send_wrapper(message: Message) -> None:
-            if generated_new_token and message["type"] == "http.response.start":
+            if generated_new_token and message.get("type") == "http.response.start":
                 response_headers = MutableHeaders(scope=message)
                 cookie = http.cookies.SimpleCookie()
                 cookie[self.cookie_name] = token
@@ -90,6 +91,39 @@ class FormCSRFMiddleware(_BaseCSRFMiddleware):
     Extend ``starlette-csrf`` middleware to also accept tokens submitted
     via form fields or JSON bodies in addition to the ``X-CSRFToken`` header.
     """
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        if scope["type"] not in ("http", "websocket"):  # pragma: no cover
+            await self.app(scope, receive, send)
+            return
+
+        request = Request(scope)
+        csrf_cookie = request.cookies.get(self.cookie_name)
+
+        needs_csrf = self._url_is_required(request.url) or (
+            request.method not in self.safe_methods
+            and not self._url_is_exempt(request.url)
+            and self._has_sensitive_cookies(request.cookies)
+        )
+
+        if needs_csrf:
+            buffered_messages = await self._buffer_request_messages(receive)
+            csrf_request = Request(scope, receive=self._replay_messages(buffered_messages))
+            submitted_csrf_token = await self._get_submitted_csrf_token(csrf_request)
+
+            if (
+                not csrf_cookie
+                or not submitted_csrf_token
+                or not self._csrf_tokens_match(csrf_cookie, submitted_csrf_token)
+            ):
+                response = self._get_error_response(csrf_request)
+                await response(scope, self._replay_messages(buffered_messages), send)
+                return
+
+            receive = self._replay_messages(buffered_messages)
+
+        send = functools.partial(self.send, send=send, scope=scope)
+        await self.app(scope, receive, send)
 
     async def _get_submitted_csrf_token(self, request: Request) -> Optional[str]:
         header_token = request.headers.get(self.header_name)
@@ -119,3 +153,31 @@ class FormCSRFMiddleware(_BaseCSRFMiddleware):
             return query_token
 
         return None
+
+    async def _buffer_request_messages(self, receive: Receive) -> List[Message]:
+        messages: List[Message] = []
+
+        while True:
+            message = await receive()
+            messages.append(message)
+
+            if message.get("type") != "http.request":
+                break
+
+            if not message.get("more_body", False):
+                break
+
+        return messages
+
+    def _replay_messages(self, messages: List[Message]) -> Receive:
+        messages_iter = iter(messages)
+
+        async def _receive() -> Message:
+            try:
+                message = next(messages_iter)
+            except StopIteration:
+                return {"type": "http.request", "body": b"", "more_body": False}
+
+            return message
+
+        return _receive
